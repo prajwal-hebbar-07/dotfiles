58fcb4c3e2ab0dd6fe3eb43e81804feb2b28be39
hebbar <prajwalhebbaras@gmail.com>
2024-11-10 19:49:38 +0530

    options change nvim

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,6 +3,64 @@ vim.keymap.set("n", ";", ":", { noremap = true })
 
 local keymap = vim.keymap
 
+keymap.set("n", "4", "$", {})
+keymap.set("n", "5", "%", {})
+
+keymap.set("n", "<M-h>", "b")
+keymap.set("n", "<M-l>", "w")
+keymap.set("i", "<M-h>", "<C-o>b")
+keymap.set("i", "<M-l>", "<C-o>w")
+
+keymap.set("n", "<C-s>", ":w<CR>")
+keymap.set("i", "<C-s>", "<ESC>:w<CR>")
+
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", {})
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", {})
+
+keymap.set("n", "<c-k>", ":wincmd k<CR>")
+keymap.set("n", "<c-j>", ":wincmd j<CR>")
+keymap.set("n", "<c-h>", ":wincmd h<CR>")
+keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+opt.cursorline = false
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+opt.linespace = 4
+
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", {})
