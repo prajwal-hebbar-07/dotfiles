4c80975930083818a5db647638ddcb45737d1f3c
hebbar <prajwalhebbaras@gmail.com>
2024-11-11 14:41:23 +0530

    nvim todo-comments update

---
+++ nvim/lua/plugins/todo.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {}, -- Plugin configuration options go here
+	config = function()
+		require("todo-comments").setup({})
+		-- Keymaps should be set after setup
+		vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { noremap = true, silent = true })
+	end,
+}
