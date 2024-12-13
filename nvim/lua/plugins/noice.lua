d14daf6a25a9638bb3ce2b47aad8d5870502515b
hebbar <prajwalhebbaras@gmail.com>
2024-12-13 13:31:52 +0530

    update neo-tree and noice
    - Updated the neo-tree plugin
    - Updated the noice plugin
    - Deleted the nvim-tree file

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,10 +1,146 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
 	},
-}
+	config = function()
+		require("noice").setup({
+			cmdline = {
+				enabled = true,
+				view = "cmdline_popup",
+				opts = { buf_options = { filetype = "vim" } },
+				format = {
+					cmdline = { pattern = "^:", icon = "", lang = "vim" },
+					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+					filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+					lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+					help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋗" },
+				},
+			},
 
+			messages = {
+				enabled = true,
+				view = "notify",
+				view_error = "notify",
+				view_warn = "notify",
+				view_history = "messages",
+				view_search = "virtualtext",
+			},
+
+			popupmenu = {
+				enabled = true,
+				backend = "nui",
+				kind_icons = {},
+			},
+
+			-- Enhanced LSP progress display
+			lsp = {
+				progress = {
+					enabled = true,
+					format = "lsp_progress",
+					format_done = "lsp_progress_done",
+					throttle = 1000 / 30, -- 30fps
+					view = "mini",
+				},
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+				hover = {
+					enabled = true,
+					silent = false,
+					view = nil,
+					opts = {},
+				},
+				signature = {
+					enabled = true,
+					auto_open = {
+						enabled = true,
+						trigger = true,
+						luasnip = true,
+						throttle = 50,
+					},
+					view = nil,
+					opts = {},
+				},
+				message = {
+					enabled = true,
+					view = "notify",
+					opts = {},
+				},
+			},
+
+			views = {
+				cmdline_popup = {
+					position = {
+						row = "50%",
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+					},
+				},
+			},
+
+			routes = {
+				-- Skip duplicate messages
+				{
+					filter = {
+						event = "msg_show",
+						kind = "",
+						find = "written",
+					},
+					opts = { skip = true },
+				},
+				-- Route long messages to split
+				{
+					filter = {
+						event = "msg_show",
+						min_height = 20,
+					},
+					view = "split",
+				},
+			},
+		})
+
+		-- Keymaps
+		vim.keymap.set("n", "<leader>nh", function()
+			require("noice").cmd("history")
+		end, { desc = "Noice History" })
+
+		vim.keymap.set("n", "<leader>nl", function()
+			require("noice").cmd("last")
+		end, { desc = "Noice Last Message" })
+
+		vim.keymap.set("n", "<leader>nd", function()
+			require("noice").cmd("dismiss")
+		end, { desc = "Dismiss Messages" })
+	end,
+}
