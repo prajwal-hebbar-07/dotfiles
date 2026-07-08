c8ff842960c750a5ebd1f70c93074733220181cb
hebbar <prajwalhebbaras@gmail.com>
2026-07-08 10:02:58 +0530

    Update dotfiles configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,11 +2,10 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
-
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-
+config.color_scheme = "rose-pine"
+config.colors = {
+  background = "#000000",
+}
 config.font = wezterm.font_with_fallback({
   "JetBrains Mono",
   "Symbols Nerd Font Mono",
@@ -14,5 +13,25 @@ config.font = wezterm.font_with_fallback({
 config.font_size = 13.0
 
 config.line_height = 1.12
+config.window_background_opacity = 0.58
+config.macos_window_background_blur = 20
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 5,
+}
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.automatically_reload_config = true
+config.audible_bell = "Disabled"
+config.adjust_window_size_when_changing_font_size = false
+config.harfbuzz_features = { "calt=0" }
+
+config.max_fps = 120
+config.animation_fps = 120
+config.front_end = "WebGpu"
 
 return config
